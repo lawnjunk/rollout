e11f8760636c66db284b2edb04391670fe8fb82c
duncan marsh <slugbyte@slugbyte.com>
2017-07-07 19:12:02 +0000

    added header log

--- route/hook-router.js
+++ route/hook-router.js
@@ -6,6 +6,7 @@ module.exports = (app) => {
   app.post('/hook/:repo', jsonParser, (req, res, next) => {
     console.log('url', req.url, '\n')
     console.log('params', req.params, '\n')
+    console.log('headers', req.headers, '\n')
     console.log('query', req.query, '\n')
     console.log('body', req.body, '\n')
     res.json({msg: 'hello world'})

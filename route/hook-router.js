69d56171ed3173759d5699f81deb0190e4d75883
Duncan Marsh <dwolfm@gmail.com>
2017-07-07 11:59:14 -0700

    added hook router route

--- route/hook-router.js
+++ route/hook-router.js
@@ -1,15 +1,15 @@
 'use strict'
 
-const router = require('../lib/router.js')
-const bodyParser = require('bodyParser')
+const jsonParser = require('body-parser').json()
 
+module.exports = (app) => {
+  app.post('/hook/:repo', jsonParser, (req, res, next) => {
+    console.log('url', req.url, '\n')
+    console.log('params', req.params, '\n')
+    console.log('query', req.query, '\n')
+    console.log('body', req.body, '\n')
+    res.json({msg: 'hello world'})
+  })
+}
 
-
-module.exports = router({
-  get: {
-    '/hook/:repository': (req, res, next) => {
-      res.json({cool: 'beans'})
-    },
-  },
-})
 

39932fb2ca044252e98858ce9f21cb2b30543044
Duncan Marsh <dwolfm@gmail.com>
2017-07-07 13:51:12 -0700

    update hook router

--- route/hook-router.js
+++ route/hook-router.js
@@ -1,16 +1,39 @@
 'use strict'
 
+const fs = require('fs-extra')
 const jsonParser = require('body-parser').json()
+const execTasks = require('../lib/exec-tasks.js')
+const hookRouter = require('../lib/hook-router.js')
+
+let TASK_DIR = `${__dirname}/../task`
+
+hookRouter.add('push', 'example', (req, res, next) => {
+  console.log(':issess:slugbyte req.body', req.body)
+  execTasks('push', 'example')
+  .then(results => res.json(results))
+  .catch(next)
+})
+
+hookRouter.add('push', 'slugbyte', (req, res, next) => {
+  res.send({msg: 'lulwat boooyea'})
+})
+
+
+TASK_DIR = '/Users/slugbyte/gitz/workspace/rollout/task' 
+
+fs.readdir(TASK_DIR).then(console.log)
+
+//
+  //return Promise.all(repos.map(repo => 
+    //fs.readdir(`${TASK_DIR}/${repo}`)
+    //.then(hooks => {
+      //return { repo, hooks }
+    //})))
+
 
 module.exports = (app) => {
   app.post('/hook/:repo', jsonParser, (req, res, next) => {
-    console.log('url', req.url, '\n')
-    console.log('params', req.params, '\n')
-    console.log('headers', req.headers, '\n')
-    console.log('query', req.query, '\n')
-    console.log('body', req.body, '\n')
-    res.json({msg: 'hello world'})
+    let event = req.headers['x-github-event']
+    hookRouter.route(event, req.params.repo)(req, res, next)
   })
 }
-
-
